551482943581a8505318380cd34d46bdddf88982
Riyaz Bahadur <rb332173@gmail.com>
2023-06-07 16:01:28 +0530

    Scrapped best fiction book

--- scrapeLogic.js
+++ scrapeLogic.js
@@ -17,20 +17,35 @@ const scrapeLogic = async (res) => {
   try {
     const page = await browser.newPage();
 
-    await page.goto("https://www.amazon.com/charts/2023-06-04/mostread/fiction");
+    await page.goto("https://www.amazon.com/charts/2023-06-04/mostsold/fiction");
 
     // Set screen size
     await page.setViewport({ width: 1080, height: 1024 });
     
-    const f = await page.$(".kc-rank-card-title")
-   //obtain text
-   const text = await (await f.getProperty('textContent')).jsonValue()
-   console.log("Text is: " + text)
+    // const f = await page.$(".kc-vertical-rank-container")
+   await page.$(".kc-vertical-rank-container")
+
+    const innerTexts = await page.evaluate(
+      () => {
+        const nodes = [...document.querySelectorAll('.kc-rank-card')];
+        const texts = nodes.map(node => [
+          node.querySelector('.kc-wol')?.innerText,
+          node.querySelector('.kc-rank-card-title')?.innerText,
+          node.querySelector('.kc-rank-card-author')?.innerText,
+          node.querySelector('.kc-rank-card-publisher')?.innerText,
+          node.querySelector('a.kc-cover-link')?.getAttribute('href'),
+          node.querySelector('a.kc-cover-link>img')?.getAttribute('src')
+        ]);
+        return texts;
+      }
+    );
+    console.log(innerTexts);
 
     // Print the full title
-    const logStatement = `The title of this blog post is ${text}`;
-    console.log(logStatement);
-    res.send(logStatement);
+    // const logStatement = `The title of this blog post is ${text}`;
+    // console.log(logStatement);
+    // res.send(logStatement);
+    res.send(innerTexts);
   } catch (e) {
     console.error(e);
     res.send(`Something went wrong while running Puppeteer: ${e}`);
